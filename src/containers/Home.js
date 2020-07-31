7d8efbcd69cb011cdec060bf4a1213739accc621
me-here <mihirthanekar@gmail.com>
2020-07-31 13:11:15 -0700

    Blocked out homepage video + coming soon sections

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,8 +5,20 @@ function Home() {
 
   return (
     <div className='homepage'>
-      <h1>Welcome to the official webpage of the Illuminate Podcast!!</h1>
-      
+      <section className='latestPodcasts'>
+        <h1>LATEST PODCASTS</h1>
+        <div className='videoContainer'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <a href="">See More</a>
+      </section>
+
+      <section className='comingSoon'>
+        <i>New Podcasts every Monday</i>
+        <strong>Also available on Apple MUSIC</strong>
+      </section>
     </div>
   );
 }
